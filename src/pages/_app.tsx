8e9b48aca90d7236daf03abb9c2c8546d5e60360
Athanasios Fytilis <thanasisfyt@gmail.com>
2025-07-13 17:14:22 +0300

    Force overwrite with correct local code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,27 +1,31 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
+import type { AppProps } from 'next/app'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Toaster } from "@/components/ui/toaster"
+import { Toaster as Sonner } from "@/components/ui/sonner"
+import { TooltipProvider } from "@/components/ui/tooltip"
+// Αν το αρχείο globals.css υπάρχει στο 'src/styles/globals.css', άφησε την παρακάτω γραμμή.
+// Αλλιώς, σβήστην.
+import '@/app/globals.css' 
 
+// Δημιουργούμε το client μία φορά έξω από το component για καλύτερη απόδοση.
 const queryClient = new QueryClient();
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+// Αυτό είναι το βασικό App component για το Next.js
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        
+        {/* Το Next.js θα φορτώσει τη σωστή σελίδα (Index, About, κλπ.) εδώ */}
+        <Component {...pageProps} />
 
-export default App;
+        {/* Τα Toasters βρίσκονται εδώ για να εμφανίζονται πάνω από κάθε σελίδα */}
+        <Toaster />
+        <Sonner />
+        
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default MyApp;
