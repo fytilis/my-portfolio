235bf30a3bd096ea3241ac68719cf34ce1543812
Athanasios Fytilis <thanasisfyt@gmail.com>
2025-07-13 16:14:13 +0300

    Fix: Use named imports for components

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,8 +1,8 @@
-import { useLocation } from "react-router-dom";
+
 import { useEffect } from "react";
 
 const NotFound = () => {
-  const location = useLocation();
+
 
   useEffect(() => {
     console.error(
